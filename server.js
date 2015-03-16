c187e847893ae9483da71bdaa21a65e109bc185c
Andre Leith <andre.leith@gmail.com>
2015-03-16 14:08:36 -0400

    Updated README.md

--- server.js
+++ server.js
@@ -13,20 +13,20 @@ var init = require('./config/init')(),
  */
 
 // Bootstrap db connection
-var mongodbUri = 'mongodb://heroku_app1234:random_password@ds029017.mongolab.com:29017/heroku_app1234';
-var mongooseUri = uriUtil.formatMongoose(mongodbUri);
-var db = mongoose.connect(mongooseUri, function(err) {
-  if (err) {
-    console.error(chalk.red('Could not connect to MongoDB!'));
-    console.log(chalk.red(err));
-  }
-});
-// var db = mongoose.connect(config.db, function(err) {
-// 	if (err) {
-// 		console.error(chalk.red('Could not connect to MongoDB!'));
-// 		console.log(chalk.red(err));
-// 	}
+// var mongodbUri = 'mongodb://heroku_app1234:random_password@ds029017.mongolab.com:29017/heroku_app1234';
+// var mongooseUri = uriUtil.formatMongoose(mongodbUri);
+// var db = mongoose.connect(mongooseUri, function(err) {
+//   if (err) {
+//     console.error(chalk.red('Could not connect to MongoDB!'));
+//     console.log(chalk.red(err));
+//   }
 // });
+var db = mongoose.connect(config.db, function(err) {
+	if (err) {
+		console.error(chalk.red('Could not connect to MongoDB!'));
+		console.log(chalk.red(err));
+	}
+});
 
 // Init the express application
 var app = require('./config/express')(db);
